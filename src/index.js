30ac7d79f63e347a3412ffe5ae18d3da2e7516f6
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-06-09 10:59:15 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+
+ReactDOM.render( <App />, document.querySelector('#root'));
+
+
+
