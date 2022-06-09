52729170c601d16b6b3206afc22a7ae74c2abe06
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-06-09 12:56:53 +0200

    Fixed templates and added components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import List from '../List/List';
+import styles from './App.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <main className="app-content">
-        <h1>My first React app</h1>
-        <h2>Hello World!</h2>
+      <main className={styles.component}>
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello World!</h2>
+        <List />
       </main>
     )
   }
